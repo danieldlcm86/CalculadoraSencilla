68e4bea8c11b95384023e36714ad7c13a8006f40
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2022-12-27 18:11:46 -0600

    Calculadora sencilla con estilos de bootstrap

--- calculadora.js
+++ calculadora.js
@@ -1,51 +1,49 @@
 //Inputs
-var variableInput1 = document.querySelector("#input1");
-var variableInput2 = document.querySelector("#input2");
+var variableInput1 = document.getElementById("input1");
+var variableInput2 = document.getElementById("input2");
 
 
 //Botones
-var botonSumar = document.querySelector("#sumar");
-var botonRestar = document.querySelector("#restar");
-var botonMultiplicar = document.querySelector("#multiplicar");
-var botonDividir = document.querySelector("#dividir");
-var botonReset = document.querySelector("#reset");
+const botonSumar = document.getElementById("sumar");
+const botonRestar = document.getElementById("restar");
+const botonMultiplicar = document.getElementById("multiplicar");
+const botonDividir = document.getElementById("dividir");
+const botonReset = document.getElementById("reset");
 
 //Resultado
-var resultado = document.querySelector("#resultado");//h2
-var valorResultado = 0;
-//var inputReset = 0;
+var resultado = document.getElementById("resultado");//h2
+var inputReset = 0;
 
 //Funciones +, -, *, /
 
 function suma(){
-    valorResultado = Number(variableInput1.value) + Number(variableInput2.value);
-    resultado.innerHTML = valorResultado.toFixed(4);
+    let valorResultado = Number(variableInput1.value) + Number(variableInput2.value);
+    resultado.innerHTML = valorResultado.toFixed(2);
 }
 botonSumar.addEventListener("click", suma);
 
 function resta(){
-    valorResultado = Number(variableInput1.value) - Number(variableInput2.value);
-    resultado.innerHTML = valorResultado.toFixed(4);
+    let valorResultado = Number(variableInput1.value) - Number(variableInput2.value);
+    resultado.innerHTML = valorResultado.toFixed(2);
 }
 botonRestar.addEventListener("click", resta);
 
 function multiplicacion(){
-    valorResultado = Number(variableInput1.value) * Number(variableInput2.value);
-    resultado.innerHTML = valorResultado.toFixed(4);
+    let valorResultado = Number(variableInput1.value) * Number(variableInput2.value);
+    resultado.innerHTML = valorResultado.toFixed(2);
 }
 botonMultiplicar.addEventListener("click", multiplicacion);
 
 function division(){
-    valorResultado = Number(variableInput1.value) / Number(variableInput2.value);
-    resultado.innerHTML = valorResultado.toFixed(4);
+    let valorResultado = Number(variableInput1.value) / Number(variableInput2.value);
+    resultado.innerHTML = valorResultado.toFixed(2);
 }
 botonDividir.addEventListener("click", division);
 
 //Función reset
 function reset(){
-    valorResultado = 0;
     variableInput1.value="";
     variableInput2.value="";
-    resultado.innerHTML = valorResultado;
+    resultado.innerHTML=inputReset;
 }
 botonReset.addEventListener("click", reset);
